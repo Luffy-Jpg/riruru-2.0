b5520451be30da161d3a8d08139551be8728e8bf
Arju <152031111+thearju@users.noreply.github.com>
2024-03-25 08:27:37 +0530

    Update chatbot.js

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -3,7 +3,7 @@ const {
         getJson,
         config,
         mode
-} = require('../lib');
+} = require('../lib-2');
 
 
 inrl({
@@ -11,19 +11,19 @@ inrl({
         fromMe: mode
 }, async (m, match) => {
         //if(m.isCreator) return;
-        if(config.CHATBOT == 'true') {
+        if(arju.CHATBOT == 'true') {
                 let data = await getJson(
-                        `http://api.brainshop.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
+                        `http://api.brainshop.ai/get?bid=${arju.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
                 )
                 return await m.reply(data.cnt)
-        } else if(config.CHATBOT == 'group' && m.isGroup) {
+        } else if(arju.CHATBOT == 'group' && m.isGroup) {
                 let data = await getJson(
-                        `http://api.brainshop.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
+                        `http://api.brainshop.ai/get?bid=${arju.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
                 )
                 return await m.reply(data.cnt)
-        } else if(config.CHATBOT == 'pm' && !m.isGroup) {
+        } else if(arju.CHATBOT == 'pm' && !m.isGroup) {
                 let data = await getJson(
-                        `http://api.brainshop.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
+                        `http://api.brainshop.ai/get?bid=${arju.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
                 )
                 return await m.reply(data.cnt)
         }
