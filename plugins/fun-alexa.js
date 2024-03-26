26301361b6e249277b904c38f38c551cf3786343
Arju <152031111+thearju@users.noreply.github.com>
2024-03-26 17:11:10 +0530

    Update fun-alexa.js

--- plugins/fun-alexa.js
+++ plugins/fun-alexa.js
@@ -10,8 +10,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
   const msg = encodeURIComponent(text);
   
-  const res = await fetch(`https://ultimetron.guruapi.tech/rekha?prompt=${msg}`);
+  const res = await fetch(`http://api.brainshop.ai/get?bid=181061&key=6xgN0zYtTefMOSKo&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]}`);
 
+
+  
   const json = await res.json();
   
   
