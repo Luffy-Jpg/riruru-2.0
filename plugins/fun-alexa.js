cc8f388fef3476089de11589dbff79da243d2009
Arju <152031111+thearju@users.noreply.github.com>
2024-03-26 17:19:49 +0530

    Update fun-alexa.js

--- plugins/fun-alexa.js
+++ plugins/fun-alexa.js
@@ -6,11 +6,11 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     throw `Hi *${name}*, do you want to talk? Respond with *${usedPrefix + command}* (your message)\n\n📌 Example: *${usedPrefix + command}* Hi bot`;
   }
   
-  m.react('🗣️');
+  m.react('🤖');
 
   const msg = encodeURIComponent(text);
   
-  const res = await fetch(`http://api.brainshop.ai/get?bid=181061&key=6xgN0zYtTefMOSKo&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]}`);
+  const res = await fetch(`http://api.brainshop.ai/get?bid=181061&key=6xgN0zYtTefMOSKo&msg=${msg}`);
 
 
   
@@ -22,9 +22,9 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 };
 
-handler.help = ['bot'];
-handler.tags = ['fun'];
-handler.command = ['bot', 'alexa'];
+handler.help = ['jarvis'];
+handler.tags = ['DAS'];
+handler.command = ['bot', 'jarvis'];
 
 export default handler;
 
