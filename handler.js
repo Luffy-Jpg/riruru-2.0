a11f0948e608cb77020774436563b1cf28d6e6fe
Arju <152031111+thearju@users.noreply.github.com>
2024-03-26 20:18:26 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -147,7 +147,7 @@ export async function handler(chatUpdate) {
                 if (!("viewOnce" in chat)) chat.viewOnce = false
                 if (!("viewStory" in chat)) chat.viewStory = false
                 if (!("welcome" in chat)) chat.welcome = false
-                if (!("chatbot" in chat)) chat.chatbot = true 
+                if (!("chatbot" in chat)) chat.chatbot = false 
                 if (!isNumber(chat.expired)) chat.expired = 0
             } else
                 global.db.data.chats[m.chat] = {
@@ -170,7 +170,7 @@ export async function handler(chatUpdate) {
                     viewOnce: false,
                     viewStory: false,
                     welcome: false,
-                    chatbot: true 
+                    chatbot: false 
                 }
           
                 
