1bfd215329e879b21c08750c7d81963778f9db2e
Arju <152031111+thearju@users.noreply.github.com>
2024-03-29 08:22:27 +0530

    Update arju-premlist.js

--- plugins/arju-premlist.js
+++ plugins/arju-premlist.js
@@ -1,5 +1,5 @@
 let handler = async m => m.reply(`
-*Welcome to permanent premium member list.*\n\n(1) Jeff ( RDJ) 🇮🇳\n(2) Mayuri 🇮🇳\n(3) Nationalist (Mashal) 🇵🇰\n(4) Faraz 🇵🇰\n(5) Alan (Hizbulla) 🇵🇰\n(6) Tushar 🇮🇳\n(7) Karan gupta 🇮🇳\n(8) Muskan 🇮🇳\n(9) Payal Jain 🇮🇳\n(10) Priya jain ( elder sister of Arju) 🇮🇳\n(11) ARJU 🇮🇳\n(12) Megha upadhyay 🇮🇳\n(13) Sai sharma 🇮🇳\n\n\n
+*Welcome to permanent premium member list.*\n\n(1) Jeff ( RDJ) 🇮🇳\n(2) Mayuri 🇮🇳\n(3) Nationalist (Mashal) 🇵🇰\n(4) Faraz 🇵🇰\n(5) Alan (Hizbulla) 🇵🇰\n(6) Tushar 🇮🇳\n(7) Karan gupta 🇮🇳\n(8) Muskan 🇮🇳\n(9) Payal Jain 🇮🇳\n(10) Priya jain ( elder sister of Arju) 🇮🇳\n(11) ARJU 🇮🇳\n(12) Megha upadhyay 🇮🇳\n(13) Sai sharma 🇮🇳\n(14) Theekshana Nirmal 🇱🇰\n\n\n
 *This bot is created by Gurucharan sahoo, Ash and Arju Sonwani.* 
 `.trim())
 handler.help = ['prem-list']
