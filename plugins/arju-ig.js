052aa0fb63371ca559d74c4d464221da088eba33
Arju <152031111+thearju@users.noreply.github.com>
2024-04-22 19:45:07 +0530

    Update arju-ig.js

--- plugins/arju-ig.js
+++ plugins/arju-ig.js
@@ -39,6 +39,6 @@ handler.help = ['instagram'];
 handler.tags = ['downloader'];
 handler.command = /^(instagram|igdl|ig|insta)$/i;
 handler.group = true;
-handler.premium = false;
+handler.premium = true;
 handler.register = true;
 export default handler;
