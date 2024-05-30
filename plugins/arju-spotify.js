117d6c39bdc48f27321d0bba4d1595d05dc4e36c
Arju <152031111+thearju@users.noreply.github.com>
2024-05-31 01:05:56 +0530

    Update arju-spotify.js

--- plugins/arju-spotify.js
+++ plugins/arju-spotify.js
@@ -1,44 +1,45 @@
-import fetch from 'node-fetch';
-
+import fetch from 'node-fetch'
+import displayLoadingScreen from '../lib/loading.js'
 let handler = async (m, { conn, text }) => {
-    if (!text) {
-        console.log('No song name provided.');
-        throw `*Please enter a song name or spotify music link!*`;
-    }
-  m.react('🤗')
-  
-  let pp = 'https://i.postimg.cc/m2rfXy5Y/IMG-20231202-WA0186.jpg'
-    const query = encodeURIComponent(text);
-    let res = `https://guruapi.tech/api/spotifydl?url=${query}`
-   // let spotify = await (await fetch(res)).buffer()
-    let doc = {
-        audio: {
-          url: res
-        },
-        mimetype: 'audio/mpeg',
-        ptt: true,
-        waveform:  [100, 0, 100, 0, 100, 0, 100],
-        fileName: "arju.mp3",
-    
-        contextInfo: {
-          mentionedJid: [m.sender],
-          externalAdReply: {
-            title: "↺ |◁   II   ▷|   ♡",
-            body: `Spotify play: ${text}`,
-            thumbnailUrl: pp,
-            sourceUrl: "https://bot-support.vercel.app",
-            mediaType: 1,
-            renderLargerThumbnail: true
-          }
-        }
-    };
-    
-    await conn.sendMessage(m.chat, doc, { quoted: m });
+  if (!text) {
+    console.log('No song name provided.')
+    throw `*Please enter a song name*`
+  }
+  m.react('🎶')
+  await displayLoadingScreen(conn, m.chat)
+  let pp = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAVeju5sp9FM0iRkRrkDBEa8y0W71XmGFw40RoaVXBKjPYrunyGLmTbDw&s=10'
+  const query = encodeURIComponent(text)
+  let res = `https://guruapi.tech/api/spotifydl?url=${query}`
+  // let spotify = await (await fetch(res)).buffer()
+    let Riruru = `https://bot-support.vercel.app`
+  let doc = {
+    audio: {
+      url: res,
+    },
+    mimetype: 'audio/mpeg',
+    ptt: true,
+    waveform: [100, 0, 100, 0, 100, 0, 100],
+    fileName: 'Riruru.mp3',
+
+    contextInfo: {
+      mentionedJid: [m.sender],
+      externalAdReply: {
+        title: '↺ |◁   II   ▷|   ♡',
+        body: `Now playing: ${text}`,
+        thumbnailUrl: pp,
+        sourceUrl: Riruru,
+        mediaType: 1,
+        renderLargerThumbnail: true,
+      },
+    },
+  }
+
+  await conn.sendMessage(m.chat, doc, { quoted: m })
 }
-handler.help = ['spotify'];
-handler.tags = ['downloader'];
-handler.command = /^(spotify|song)$/i;
-handler.premium = true;
-handler.group = true;
-handler.register = true;
-export default handler;
+handler.help = ['spotify']
+handler.tags = ['downloader']
+handler.command = /^(spotify|song)$/i
+handler.group = true 
+handler.premium = true
+
+export default handler
