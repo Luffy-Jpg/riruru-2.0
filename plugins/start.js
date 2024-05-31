b428bce7d023ac6208d46b28ec57460ea648558c
Arju <152031111+thearju@users.noreply.github.com>
2024-05-31 16:48:12 +0530

    Update start.js

--- plugins/start.js
+++ plugins/start.js
@@ -1,17 +1,17 @@
 let handler = async (m) =>
   m.reply(
     `
-    \n${username}\nWelcome to Riruru Chat interface
+    \n\nWelcome to Riruru Chat interface
 
-Basic knowledge about this bot to how to use it.\n(1) *.help* or *.menu* To use the commands.\n(2) *Riruru-Chatbot* is only for *LEGENDS.*\n(3) If you want to add this bot in your WhatsApp group, please send an invite link to the Bot *ADMIN.*\n(4) This bot is in development.\n(5) Note: Don't misuse!
+Basic knowledge about this bot to how to use it.\n(1) *.help* or *.menu* To use the commands.\n\n(2) *Riruru-Chatbot* is only for *LEGENDS.*\n\n(3) If you want to add this bot in your WhatsApp group, please send an invite link to the Bot *ADMIN.*\n\n(4) This bot is in development.\n\n(5) Join Our BOT-HUB to use the bots \n\n> URL: *https://chat.whatsapp.com/KlSW8y9vDAh5ism0aN4i7a*\n\n(6) For more information you can chat with my Admin.
 
 
 \n> *Admin : ARJU*\n> *Contact : wa.me/919131652091*\n> *Website: https://bot-support.vercel.app*
 
 `.trim(),
   );
-let user = global.db.data.users[who];
-let username = conn.getName(who);
+//let user = global.db.data.users[who];
+//let username = conn.getName(who);
 handler.help = ["start"];
 handler.tags = ["BOT-SUPPORT"];
 handler.command = ["start"];
