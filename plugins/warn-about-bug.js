694f74874086b479a08ce33c3c21aecb1cb5dc21
Arju <152031111+thearju@users.noreply.github.com>
2024-06-08 01:52:19 +0530

    Update warn-about-bug.js

--- plugins/warn-about-bug.js
+++ plugins/warn-about-bug.js
@@ -1,8 +1,8 @@
 let handler = async (m) =>
   m.reply(
     `
-    \n\n*Warning ⚠️*\n\nThis BUG can crash your device or WhatsApp chat. Use this Tool at your own risk.\nAdmin & Owner Required. 
-    \n\n*Get started By typing \n*.bug-start*
+    \n\n*Warning ⚠️*\n\nThis BUG can crash your device or WhatsApp chat. Use this Tool at your own risk. And you won't be able to recover chats, if u have laptop or desktop than you can recover. Mobile users please dont use this command.\nOwner Required. 
+    \n\n*Get started By typing*\n\n*.bug-start*
 `.trim(),
   );
 handler.help = ["bug"];
