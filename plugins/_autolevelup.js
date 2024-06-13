7a7fbb43c62e7b226498305e29ff3c169d59c918
Arju <152031111+thearju@users.noreply.github.com>
2024-06-14 03:34:19 +0530

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -11,7 +11,7 @@ export async function before(m, { conn }) {
     user.role = global.rpg.role(user.level).name
     if (before !== user.level) {
         m.reply(`
-	Koi mere se baat nhi karta 🥺`.trim())
+	🥺`.trim())
     }
 }
 
