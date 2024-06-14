d2f9e1515d39ec8c98e9c6d85539bf9801241c4a
Arju <152031111+thearju@users.noreply.github.com>
2024-06-15 02:35:03 +0530

    Create riruru-msg.js

---
+++ plugins/riruru-msg.js
@@ -0,0 +1,19 @@
+let handler = async (m, { conn }) => {
+  let user = global.db.data.users[m.sender]
+  let name = conn.getName(m.sender)
+  let taguser = '@' + m.sender.split('@s.whatsapp.net')[0]
+  let av = ``
+
+  m.reply(`Hello ${taguser} Need help?  type /help `)
+  conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+}
+
+handler.customPrefix = /^(bot|riruru|riraru)$/i
+handler.command = new RegExp()
+handler.group = true
+
+export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
