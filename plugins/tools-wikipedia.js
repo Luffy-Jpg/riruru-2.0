504c6f6cf7f8d18a79a7ce836bc1e5dfccbfbb08
Arju <152031111+thearju@users.noreply.github.com>
2024-06-17 00:37:27 +0530

    █▓▒▒░░░ARJU░░░▒▒▓█

--- plugins/tools-wikipedia.js
+++ plugins/tools-wikipedia.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 import cheerio from 'cheerio'
 
 let handler = async (m, { text }) => {
-  if (!text) throw `✳️ Enter what you want to search for on Wikipedia`
+  if (!text) throw `*Enter what you want to search for on Wikipedia*`
 
   try {
     const link = await axios.get(`https://es.wikipedia.org/wiki/${text}`)
     const $ = cheerio.load(link.data)
     let wik = $('#firstHeading').text().trim()
     let resulw = $('#mw-content-text > div.mw-parser-output').find('p').text().trim()
-    m.reply(`▢ *Wikipedia*
+    m.reply(`*Wikipedia*
 
 ‣ Buscado : ${wik}
 
@@ -18,8 +18,8 @@ ${resulw}`)
     m.reply('⚠️ No results found ')
   }
 }
-handler.help = ['wikipedia']
-handler.tags = ['tools']
+handler.help = ['wiki']
+handler.tags = ['TOOL']
 handler.command = ['wiki', 'wikipedia']
-
+handler.group = true
 export default handler
