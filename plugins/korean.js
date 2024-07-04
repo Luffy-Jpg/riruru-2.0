43fc88a91766d2187c1dc50097181a2ff1dd1362
Arju <152031111+thearju@users.noreply.github.com>
2024-07-05 01:39:54 +0530

    Update korean.js

--- plugins/korean.js
+++ plugins/korean.js
@@ -7,7 +7,7 @@ let endpoint = `https://shizoapi.onrender.com/api/pies/korea?apikey=shizo`
 const response = await fetch(endpoint);
 if (response.ok) {
       const imageBuffer = await response.buffer();
-      await conn.sendFile(m.chat, imageBuffer, 'bot.jpg', msg, m, null, );
+      await conn.sendFile(m.chat, imageBuffer, 'bot.jpg', msg, m, rpyt, null, );
     } else {
       throw bug
     }
